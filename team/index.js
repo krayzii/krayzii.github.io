8003fe6e0512639267fcfca9293f5d98b2140ffb
krayzii <krayziemeilee@gmail.com>
2024-06-13 20:52:12 +0000

    finished changes to team

---
+++ team/index.js
@@ -0,0 +1,9 @@
+function goToProgress() {
+    document.getElementById("0").style.display = "none";
+    document.getElementById("1").style.display = "flex";
+}
+
+function goToMain() {
+    document.getElementById("0").style.display = "flex";
+    document.getElementById("1").style.display = "none";
+}
