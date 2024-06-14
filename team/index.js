40206cbaa02152f7a4b84c5c3e3e97bd9623eca4
krayzii <krayziemeilee@gmail.com>
2024-06-14 18:41:24 +0000

    Team Page

--- team/index.js
+++ team/index.js
@@ -1,9 +1,17 @@
 function goToProgress() {
     document.getElementById("0").style.display = "none";
     document.getElementById("1").style.display = "flex";
+    document.getElementById("2").style.display = "none";
 }
 
 function goToMain() {
     document.getElementById("0").style.display = "flex";
     document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "none";
+}
+
+function goToTeam() {
+    document.getElementById("0").style.display = "none";
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "flex";
 }
