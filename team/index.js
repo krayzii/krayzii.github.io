cc5935f6e14906164cdc299bb76110bebfb84db3
krayzii <krayziemeilee@gmail.com>
2024-06-14 19:59:29 +0000

    info to pages

--- team/index.js
+++ team/index.js
@@ -17,9 +17,11 @@ function goToTeam() {
 }
 
 function updateText(id, list) {
-    document.getElementById(id).innerText = list[Math.round(Math.random() * list.length)];
+    document.getElementById(id).innerText = list[Math.round(Math.random() * (list.length - 1))];
 }
 
-var bio_list = ["I like coding stupid things...", "Goose", "Javascript > Html", "MySql is annoying", "Coding enthusiast", "Eclipse IDE is fun", "JS > Java > PY > Lua > HTML > C#/C > SQL > CSS", "CSS sucks"];
+var bio_list = ["I like coding stupid things...", "Goose", "Javascript > Html", "MySQL is annoying", "Coding enthusiast", "Eclipse IDE is fun", "JS > Java > PY > Lua > HTML > C#/C > SQL > CSS", "CSS sucks"];
 
-setInterval(updateText("nathan_desc" , bio_list), 3000);
+function run() {
+    setInterval(updateText, 3000, "nathan_desc" , bio_list);
+}
