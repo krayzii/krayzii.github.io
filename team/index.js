368b680a9e2781dd90a1ed2cad8c5d704bda96ac
krayzii <krayziemeilee@gmail.com>
2024-06-14 19:34:46 +0000

    scroll and info

--- team/index.js
+++ team/index.js
@@ -15,3 +15,11 @@ function goToTeam() {
     document.getElementById("1").style.display = "none";
     document.getElementById("2").style.display = "flex";
 }
+
+function updateText(id, list) {
+    document.getElementById(id).innerText = list[Math.round(Math.random() * list.length)];
+}
+
+var bio_list = ["I like coding stupid things...", "Goose", "Javascript > Html", "MySql is annoying", "Coding enthusiast", "Eclipse IDE is fun", "JS > Java > PY > Lua > HTML > C#/C > SQL > CSS", "CSS sucks"];
+
+setInterval(updateText("nathan_desc" , bio_list), 3000);
