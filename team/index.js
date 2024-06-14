b44410887220a4867aabd72017fe273466d69d1d
krayzii <krayziemeilee@gmail.com>
2024-06-14 20:08:16 +0000

    updates

--- team/index.js
+++ team/index.js
@@ -2,25 +2,35 @@ function goToProgress() {
     document.getElementById("0").style.display = "none";
     document.getElementById("1").style.display = "flex";
     document.getElementById("2").style.display = "none";
+    //document.getElementById("3").style.display = "none";
 }
 
 function goToMain() {
     document.getElementById("0").style.display = "flex";
     document.getElementById("1").style.display = "none";
     document.getElementById("2").style.display = "none";
+    //document.getElementById("3").style.display = "none";
 }
 
 function goToTeam() {
     document.getElementById("0").style.display = "none";
     document.getElementById("1").style.display = "none";
     document.getElementById("2").style.display = "flex";
+    //document.getElementById("3").style.display = "none";
+}
+
+function goToMainTwo() {
+    document.getElementById("0").style.display = "none";
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "none";
+    //document.getElementById("3").style.display = "flex";
 }
 
 function updateText(id, list) {
     document.getElementById(id).innerText = list[Math.round(Math.random() * (list.length - 1))];
 }
 
-var bio_list = ["I like coding stupid things...", "Goose", "Javascript > Html", "MySQL is annoying", "Coding enthusiast", "Eclipse IDE is fun", "JS > Java > PY > Lua > HTML > C#/C > SQL > CSS", "CSS sucks"];
+var bio_list = ["I like coding stupid things...", "Goose", "Javascript > Html", "MySQL is annoying", "Coding enthusiast", "Eclipse IDE is fun", "JS, Java are fun!", "CSS sucks"];
 
 function run() {
     setInterval(updateText, 3000, "nathan_desc" , bio_list);
